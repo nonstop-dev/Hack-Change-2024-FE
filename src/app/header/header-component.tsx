f1116d4ed46499887798bf2801d24d3ed08c2d97
surgutov55 <surgutov55@gmail.com>
2024-11-30 20:22:04 +0300

    added condition in header

--- src/app/header/header-component.tsx
+++ src/app/header/header-component.tsx
@@ -2,8 +2,12 @@ import Image from 'next/image';
 import styles from './header.module.scss';
 import { InputAdornment, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const path = usePathname();
+  const isSearchPage = path === '/search';
+
   return (
     <header className={styles.header}>
       <div className={styles.left}>
@@ -11,21 +15,25 @@ export default function Header() {
         <h1 className={styles.heading}>ПОРТАЛ</h1>
       </div>
       <div className={styles.headerRight}>
-        <TextField
-          placeholder="Поиск"
-          slotProps={{
-            input: {
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon></SearchIcon>
-                </InputAdornment>
-              ),
-              sx: { borderRadius: 16 },
-            },
-          }}
-          size="small"
-          variant="outlined"
-        />
+        {isSearchPage ? (
+          ''
+        ) : (
+          <TextField
+            placeholder="Поиск"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon></SearchIcon>
+                  </InputAdornment>
+                ),
+                sx: { borderRadius: 16 },
+              },
+            }}
+            size="small"
+            variant="outlined"
+          />
+        )}
       </div>
     </header>
   );
