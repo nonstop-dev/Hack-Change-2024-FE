8f07cb2a8cd3145b33ae45bbb2e99d58b6bdbaa4
surgutov55 <surgutov55@gmail.com>
2024-12-01 03:20:58 +0300

    added redirect from search to empty page

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from 'next';
 
+module.exports = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: '/search',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
